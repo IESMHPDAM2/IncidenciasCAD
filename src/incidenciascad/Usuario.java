e92ae713c46c2069f276fa4c4f5a2fec099bb12a
ifontecha <ifontecha@MySQL2008.iesmhp.local>
2017-11-07 15:55:44 +0100

    Versión inicial

---
+++ src/incidenciascad/Usuario.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package incidenciascad;
+
+/**
+ *
+ * @author ifontecha
+ */
+public class Usuario {
+    private int usuarioId;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private Departamento departamento;
+
+    public Usuario() {
+    }
+
+    public Usuario(int usuarioId, String nombre, String apellido1, String apellido2, Departamento departamento) {
+        this.usuarioId = usuarioId;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.departamento = departamento;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    
+    
+}
